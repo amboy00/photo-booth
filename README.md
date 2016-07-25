7fdbd2d2e58e320f2af0d7ecdb6d05a8c13c15b8
Chris Williams <chris@martinilab.com>
2016-07-25 17:03:50 -0500

    updated script for ux, updated set up instructions

--- README.md
+++ README.md
@@ -2,12 +2,31 @@
 Bash script that takes photos, turns them into a gif with a caption.
 
 ## Installation
-Requires ImageMagick
+Requires ImageMagick and other packages installed via homebrew
 
 ```
+brew install imagesnap
 brew install imagemagick
 brew install jp2a
 ```
+
+### Twitter
+
+To use twitter from the command line, install the `t` gem.  And follow its setup instructions.  If your target Twitter account already has your "app" created and authorized, then you just need to reauthorize for your computer.
+
+```
+gem install t
+```
+
+### link
+
+Make a link to the shell script if you want to avoid having to type `./gifme.sh` every time.
+
+```
+$ ln -s ~/location-top/photo-booth/gifme.sh ~/bin/photobooth
+```
+
+> Note: the command `photobooth` will work, but you still need to run it from within the "photo-booth" directory to read the ASCII font. Sorry.
 
 Requires a camera on the computer to work.
 
--- gifme.sh
+++ gifme.sh
@@ -1,54 +1,82 @@
-#!/bin/sh
+#!/bin/bash
 #
 #
+
+BLACK=$(tput setaf 0)
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+YELLOW=$(tput setaf 3)
+LIME_YELLOW=$(tput setaf 190)
+POWDER_BLUE=$(tput setaf 153)
+BLUE=$(tput setaf 4)
+MAGENTA=$(tput setaf 5)
+CYAN=$(tput setaf 6)
+WHITE=$(tput setaf 7)
+BRIGHT=$(tput bold)
+NORMAL=$(tput sgr0)
+BLINK=$(tput blink)
+REVERSE=$(tput smso)
+UNDERLINE=$(tput smul)
+
 clear
-echo "Warming up the camera!\n"
+# figlet "Welcome to CLI Booth!"
+
+toilet -f 'SlantRelief.flf' 'CLI Booth' -w 200
+
+printf "\n\n\nCLI Booth is a Command Line Interface (CLI) that takes five photos and tweets the captured images as a gif.\n"
+printf "Tell who you are and @clibooth will AT you this pic!\n\n"
+printf "${RED}Please enter your twitter handle: ${NORMAL}@"
+read TWITTER
+printf "\n${RED}Enter a caption: ${NORMAL}"
+read CAPTION
+
+clear
+toilet -f 'SlantRelief.flf' 'Get ready!' -w 200
 
 # Take a bunch of photos
 for (( i=0 ; i < 5 ; i++ ))
 do
   imagesnap -w 2 snapshot-$i.jpg
   clear
-  jp2a --colors  snapshot-$i.jpg
+  jp2a --colors snapshot-$i.jpg --clear
 
-  composite -compose atop -gravity southeast FP-Logo.png snapshot-$i.jpg snapshot-$i.jpg
+  composite -compose atop -gravity southeast watermark.png snapshot-$i.jpg snapshot-$i.jpg
 
 done
 # Make a gif
 convert -dispose none -delay 50 *.jpg -scale 1024x768 -size 1024x768 source.gif
 
-wflag=
-while getopts 'w:' OPTION
-do
-  case $OPTION in
-    w)  wflag="$OPTARG"
-        ;;
-    ?)  printf "Usage: %s: [-w value]\n" $(basename $0) >&2
-        exit 2
-        ;;
-  esac
-done
-wflag+=' #FrontPorchIO'
-
 shift $(($OPTIND - 1))
 
 clear
 
-if [ "$wflag" ]
+if [ "$CAPTION" ]
 then
-  printf "Saving File with - $wflag\n"
-  convert source.gif -size 1024x768 -font /Library/Fonts/Futura.ttc -pointsize 36 -fill black -draw "text 40,34 '$wflag' fill white text 41,33 '$wflag' fill white" -loop 0 -layers Optimize -quantize RGB +dither -colors 64 output.gif
+  printf "Saving File with '$CAPTION'\n"
+  convert source.gif -size 1024x768 -font /Library/Fonts/Futura.ttc -pointsize 36 -fill black -draw "text 40,50 '$CAPTION' fill white text 38,48 '$CAPTION' fill white" -loop 0 -layers Optimize -quantize RGB +dither -colors 64 output.gif
+else
+  printf "Saving File\n"
+  CAPTION="Another great tweet."
+  convert source.gif -size 1024x768 -loop 0 -layers Optimize -quantize RGB +dither -colors 64 output.gif
 fi
+
+if [ -z "$TWITTER" ]
+then
+  TWITTER="clibooth"
+fi
+
 
 # remove image files
 find *.jpg | xargs rm
 
-echo "output.gif created. Showing in Finder."
 sleep 1
-echo "sending to twitter's @FrontPorchIO"
+printf "Tweeting to ${CYAN}@clibooth${NORMAL}\n"
 sleep 1
 
 # open .
 
-$(t update 'Showing of the Photo Booth #frontporchio' -f output.gif)
-# photo bash.  Get it?  Bash!!!
+
+$(t update "$CAPTION by @$TWITTER" -f output.gif)
+
+toilet -f 'SlantRelief.flf' 'All done!' -w 200 --gay
+printf "\n\nGo to URL ${CYAN}http://twitter.com/clibooth${NORMAL} to see your tweet!"
